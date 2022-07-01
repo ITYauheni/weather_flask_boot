ddad4e670c50633ac053a96e7c067bae5c0a27a9
ITYauheni <itkachenkayauheni@gmail.com>
2022-07-01 15:14:11 +0300

    After set webhook

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template
+from flask import request
 
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route("/", methods=["GET", "POST"])
+def receive_update():
+    if request.method == "POST":
+        print(request.json)
+    return {"ok": True}
